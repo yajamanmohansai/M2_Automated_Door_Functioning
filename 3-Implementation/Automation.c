254e1a0d67cf485c4b0b27c73c2a71bd0f9da7ee
Mohansai <101581268+yajamanmohansai@users.noreply.github.com>
2022-04-25 10:04:38 +0530

    Add files via upload

---
+++ 3-Implementation/Automation.c
@@ -0,0 +1,14 @@
+#include <avr/io.h>
+int main(void)
+{
+	DDRB=DDRB&0b11111101;
+	DDRC=DDRC|0b01000000;
+	while(1)
+	{
+		if(PINB & 0b00000010)
+		PORTC=PORTC|0b01000000;
+		else
+		PORTC=PORTC&0b10111111;
+	}
+	return 0;
+}
